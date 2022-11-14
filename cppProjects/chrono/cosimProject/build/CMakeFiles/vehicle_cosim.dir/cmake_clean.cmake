82e022f44d8dfa94f9e13c77b87c3e0545e6012d
Benjamin Johnson <johnson.benjamin.i@gmail.com>
2022-11-14 12:59:35 -0500

    first push since chrono

---
+++ cppProjects/chrono/cosimProject/build/CMakeFiles/vehicle_cosim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vehicle_cosim.dir/vehicle_cosim.cpp.o"
+  "CMakeFiles/vehicle_cosim.dir/vehicle_cosim.cpp.o.d"
+  "vehicle_cosim"
+  "vehicle_cosim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vehicle_cosim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cppProjects/chrono/cosimProject/build/CMakeFiles/vehicle_cosim.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vehicle_cosim.
+# This may be replaced when dependencies are built.
