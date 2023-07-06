2237bad276da8869694e8c6fdd3da0c637630acc
Benjamin Johnson <johnson.benjamin.i@gmail.com>
2023-07-06 09:33:43 -0400

    added non linear bicycle MPC, still needs lots of tuning and to have a variable reference trajectory

--- cppProjects/acadoExamples/bicycleModel/bicycleModel.cpp
+++ cppProjects/acadoExamples/bicycleModel/bicycleModel.cpp
@@ -1,6 +1,8 @@
 #include <acado_optimal_control.hpp>
 #include <acado_toolkit.hpp>
 #include <bindings/acado_gnuplot/gnuplot_window.hpp>
+#include <iostream>
+#include <vector>
 
 int main( )
 {
@@ -17,14 +19,18 @@ int main( )
     DifferentialState YB;
     //DifferentialState omega;
 
-    AlgebraicState alpha_f;
-    AlgebraicState alpha_r;
+    //AlgebraicState alpha_f;
+    //AlgebraicState alpha_r;
+
+    IntermediateState alpha_f;
+    IntermediateState alpha_r;
     //AlgebraicState lambda_f;
     //AlgebraicState lambda_r;
 
     Disturbance R;
-    Control delta, torque;
-    //Control torque;
+    Control delta;
+    Control torque;
+
 
     double mB = 1350.0;
     double Jz = 1200;
@@ -34,22 +40,23 @@ int main( )
     double r_tire = 0.5;
     double C_alpha = 1250;
 
-
+    alpha_f = (vB+lf*rB)/uB - delta;
+    alpha_r = (vB-lr*rB)/uB;
 
 
     // DEFINE A DIFFERENTIAL EQUATION:
     // -------------------------------
     DifferentialEquation f;
 
-    f << dot(uB) == torque/r_tire + vB*rB;
-    f << dot(vB) == C_alpha*(alpha_f + alpha_r) - uB*rB;
-    f << dot(rB) == C_alpha*(alpha_f*lf - alpha_r*lr)/Jz;
+    f << dot(uB) ==( torque/r_tire)/mB + vB*rB;
+    f << dot(vB) == (-C_alpha*(alpha_f + alpha_r))/mB - uB*rB;
+    f << dot(rB) == -C_alpha*(alpha_f*lf - alpha_r*lr)/Jz;
     f << dot(psiB) == rB;
     f << dot(XB) == uB*cos(psiB) - vB*sin(psiB);
     f << dot(YB) == uB*sin(psiB) + vB*cos(psiB);
     //f << dot(omega) == 
-    f << 0 == (vB+lf*rB)/uB - delta - alpha_f;
-    f << 0 == (vB-lr*rB)/uB - alpha_r;
+    //f << 0 == (vB+lf*rB)/uB - delta - alpha_f;
+    //f << 0 == (vB-lr*rB)/uB - alpha_r;
     //f << 0 == lambda_r - (omega*r_tire - uB)/uB;
 
 
@@ -59,7 +66,7 @@ int main( )
     OutputFcn identity;
     DynamicSystem dynamicSystem( f,identity );
 
-    Process process( dynamicSystem,INT_BDF );
+    Process process( dynamicSystem, INT_RK45 );
 
     //VariablesGrid disturbance;// = readFromFile( "road.txt" );
     //disturbance.read( "road.txt" );
@@ -84,35 +91,51 @@ int main( )
     DMatrix Q(8,8);
     Q(0,0) = 10.0;
 
-    Q(1,1) = 1.0;
-    Q(2,2) = 1.0;
-    Q(3,3) = 1.0e-3;
+    Q(1,1) = 10.0;
+    Q(2,2) = 1.0e-2;
+    Q(3,3) = 1.0e-2;
     Q(4,4) = 1.0e-2;
-    Q(5,5) = 10.0;
-    Q(6,6) = 1.0e-4;
+    Q(5,5) = 1000.0;
+    Q(6,6) = 1.0e-2;
     Q(7,7) = 1.0e-4;
 
     // Reference
     DVector r(8); 
     r.setAll( 0.0 );
     r(0) = 10;
-    r(4) = 100;
-    r(5) = 2.7;
-
+    r(4) = 210;
+    r(5) = 3.00;
+    
 
 
     // DEFINE AN OPTIMAL CONTROL PROBLEM:
     // ----------------------------------
     const double t_start = 0.0;
-    const double t_end   = 10.0;
+    const double t_end   = 1.0;
 
-    OCP ocp( t_start, t_end, 200 );
+    OCP ocp( t_start, t_end, 10 );
+    VariablesGrid data( 2, 3.0,10.0,15 );
+    data.setZero();
+    data.print();
     
 
     ocp.minimizeLSQ( Q, h, r );
-    
 
+    //Expression torquePenalty = pow(torque, 2);
+    //Expression velocityPenalty = pow(uB-10, 2);
+
+    //Expression lagrangianTerm = torquePenalty + velocityPenalty;
+    
+    //ocp.minimizeLagrangeTerm( lagrangianTerm );
+    
+    
+    //ocp.subjectTo( AT_END, XB == 50 );
     ocp.subjectTo( f );
+    ocp.subjectTo( -3*M_PI/180 <= alpha_f <= 3*M_PI/180);
+    //ocp.subjectTo( AT_START, uB == 7);
+    //ocp.subjectTo( AT_END, uB == 10);
+    //ocp.subjectTo( AT_START, torque == 0 )
+    //ocp.subjectTo( f );
     ocp.subjectTo( -20000.0 <= torque <= 20000.0 );
     //ocp.subjectTo( R == 0.0 );
     ocp.subjectTo( -0.5 <= delta <= 0.5 );
@@ -122,32 +145,49 @@ int main( )
     // ------------------------------
     RealTimeAlgorithm alg( ocp, 0.1 );
     
-    alg.set( INTEGRATOR_TYPE, INT_BDF );
-    alg.set( MAX_NUM_ITERATIONS,2 );
+    //OptimizationAlgorithm alg( ocp );
+    //MultiObjectiveAlgorithm alg( ocp );
+    
+    alg.set( INTEGRATOR_TYPE, INT_RK45 );
+    alg.set( MAX_NUM_ITERATIONS, 10 );
+    alg.set( KKT_TOLERANCE, 1e-3 );
+
 
     StaticReferenceTrajectory zeroReference;
+    //std::vector<double> ref {10,0,0,0,0,0};
+    //VariablesGrid yRef(ref);
 
-    Controller controller( alg,zeroReference );
+    //yRef.setZero();
+    //yRef(0).setAll(10.0);
+    //std::cout << yRef.getNumRows() << std::endl;
+    //std::cout << yRef.getNumCols() << std::endl;
+    //std::cout << yRef << std::endl;
 
-
-    // SETTING UP THE SIMULATION ENVIRONMENT,  RUN THE EXAMPLE...
-    // ----------------------------------------------------------
-    SimulationEnvironment sim( t_start, t_end, process, controller );
-   
+    //StaticReferenceTrajectory referenceTrajectory(yRef);
 
     DVector x0(6);
     x0.setZero();
     x0(0) = 7;
 
+    Controller controller( alg,zeroReference );
+    
+    //controller.init( 0.0,x0 );
+    //controller.step( 0.0,x0 );
+    
+    // SETTING UP THE SIMULATION ENVIRONMENT,  RUN THE EXAMPLE...
+    // ----------------------------------------------------------
+    double t_sim_end = 20;
+    SimulationEnvironment sim( t_start, t_sim_end, process, controller );
     sim.init( x0 );
     sim.run( );
-
+    
 
     // ... AND PLOT THE RESULTS
     // ------------------------
     VariablesGrid diffStates;
+    
     sim.getProcessDifferentialStates( diffStates );
-
+    
     VariablesGrid feedbackControl;
     sim.getFeedbackControl( feedbackControl );
 
@@ -156,7 +196,7 @@ int main( )
         window.addSubplot( diffStates(1),   "Lateral Velocity [m/s]" );
         window.addSubplot( diffStates(2),   "Yaw Velocity [rad/s]" );
         window.addSubplot( diffStates(3),   "Yaw [rad]" );
-        window.addSubplot( diffStates(5), "Body Position X [m]");
+        window.addSubplot( diffStates(4), "Body Position X [m]");
         window.addSubplot( diffStates(5), "Body Position Y [m]");
         window.addSubplot( feedbackControl(0), "delta (rad)" );
         window.addSubplot( feedbackControl(1),     "torque (Nm)" );
---
+++ cppProjects/acadoExamples/bicycleModel/debug/CMakeFiles/bicycleModel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bicycleModel.dir/bicycleModel.cpp.o"
+  "../bicycleModel.pdb"
+  "../bicycleModel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bicycleModel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
